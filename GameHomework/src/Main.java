fee9f476be0233cdffee9726da8773605e43efd8
madjarevicn <nikola.madjarevic@gmail.com>
2017-11-07 12:24:27 +0100

    Added method for scaling image and uploaded images to resources

--- GameHomework/src/Main.java
+++ GameHomework/src/Main.java
@@ -1,4 +1,16 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+import rafgfxlib.ImageViewer;
+import rafgfxlib.Util;
 
 public class Main {
 
+	public static void main(String[] args) {
+		BufferedImage imgMadji = Util.loadImage("src/resources/madji.jpg");
+		WritableRaster target = Util.createRaster(imgMadji.getWidth()/2, imgMadji.getHeight()/2, false);
+		
+		target = ScaleImage.scale(imgMadji, imgMadji.getWidth()/2, imgMadji.getHeight()/2).getRaster();
+		ImageViewer.showImageWindow(Util.rasterToImage(target), "RAF Racunarska Grafika");
+	}
 }

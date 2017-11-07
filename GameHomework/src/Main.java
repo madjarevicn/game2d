c935dce2410ecaea6fcf5e854b4f74f7d5cc7be7
madjarevicn <nikola.madjarevic@gmail.com>
2017-11-07 22:44:20 +0100

    Tetris

--- GameHomework/src/Main.java
+++ GameHomework/src/Main.java
@@ -7,10 +7,10 @@ import rafgfxlib.Util;
 public class Main {
 
 	public static void main(String[] args) {
-		BufferedImage imgMadji = Util.loadImage("src/resources/shon.jpg");
+		BufferedImage imgMadji = Util.loadImage("src/resources/madji.jpg");
 		WritableRaster target = Util.createRaster(imgMadji.getWidth()/2, imgMadji.getHeight()/2, false);
 		
-		target = ImageMethods.scale(imgMadji, imgMadji.getWidth()/2, imgMadji.getHeight()/2).getRaster();
+		target = ImageMethods.scale(imgMadji, imgMadji.getWidth()/4, imgMadji.getHeight()/4).getRaster();
 		
 		BufferedImage compressed = Util.rasterToImage(target);		
 		ImageViewer.showImageWindow(ImageMethods.reflect(compressed), "Madji");
